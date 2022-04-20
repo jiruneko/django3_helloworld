101e2761486671f83f0cfe9c84cd137b863810ba
jiruneko <36216683+jiruneko@users.noreply.github.com>
2022-04-21 07:24:25 +0900

    Update settings.py

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b7qch$_4@uzuz7hc^=v!g6pdnmtuoo9em1m^q1_$uj=_1%mno!'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
